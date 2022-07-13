26f8d12bf935c25bc040fb0ee6d0719112fac332
MadukaJustice <jonasjustice98@gmail.com>
2022-07-13 16:55:21 +0100

    initial commit#

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './component/button'
+
+function App() {
+      return (
+        <div>
+            <Button title='HELLO' />
+            <img src='' alt='' />
+        </div>
+      )
+  }
+
+
+
+export default App;
---
+++ src/component/button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = (props) => {
+    return(
+        <div>
+            <button>{props.title}</button>
+        </div>
+    )
+}
+export default Button;
